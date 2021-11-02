b08a8ce422eedcc8ba0598620e2e841ccb171c40
luca oliveira <luca.developerpk@gmail.com>
2021-11-02 20:36:37 -0300

    banner e mais vendidos

--- index.php
+++ index.php
@@ -6,16 +6,51 @@
 	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
 	<link rel="stylesheet" href="css/header.css">
 	<link rel="stylesheet" href="css/home.css">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
 </head>
 <body>
 	<?php  require_once("assets/header.php");?>
 
 	<main>
 		<div class="banner-home">
-			<p>Seja bem vindo(a) a ACME!</p>
-			<p>Aqui você encontra os melhores calçados pelo menor preço</p>
+			<div id="container">
+				<div class="banner-conteudo">
+					<span>Seja bem vindo(a) a ACME!</span>
+					<p>Aqui você encontra os melhores calçados<br> pelo menor preço</p>
+				</div>
+			</div>
+		</div>
+		<div class="mais-vendidos">
+			<div class="text-vendidos">
+				<p>Mais vendidos</p>
+			</div>
+			<div class="box-img-mais-vendidos">
+				
+				<div class="img-mais-vendidos">
+
+					<div id="container-img-vendidos">
+						<div class="banner-conteudo-vendidos">
+							<span>Bota de trilha - 123 Lorem Ipsum</span>
+							
+							<a href="#">Encontrar loja mais próxima</a>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="img-mais-vendidos">
+
+					<div id="container-img-vendidos">
+						<div class="banner-conteudo-vendidos">
+							<span>Tênis Nike 12345 - Lorem Ipsum</span>
+
+							<a href="#">Encontrar loja mais próxima</a>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
 		</div>
 	</main>
 </body>
