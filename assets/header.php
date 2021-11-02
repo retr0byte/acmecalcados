87044ced241ea4bfd71b1d727972aea4dd560bcd
luca oliveira <luca.developerpk@gmail.com>
2021-11-02 15:34:57 -0300

    header

---
+++ assets/header.php
@@ -0,0 +1,32 @@
+	<header>
+
+		<div class="all-menu">
+
+			<section>
+				<a href="index.php"><img src="imagens/logo.png" class="logo"></a>
+			</section>
+
+			<section class="section-itens">
+				<nav class="nav-header">
+					<ul>
+						<li class="first-li"><a href="#">HOME</a></li>
+						<li class="first-li"><a href="#">SOBRE</a>
+							<ul>
+								<li><a href="#">VISÃO, MISSÃO E VALORES</a></li>
+								<li><a href="#">CULTURA</a></li>
+								<li><a href="#">PROJETOS SOCIAIS</a></li>
+							</ul>
+						</li>
+						<li class="first-li"><a href="#">CONTATO</a></li>
+					</ul>
+				</nav>
+
+				<div class="franqueado-link">
+					<a href="#">SEJA UM FRANQUEADO</a>
+				</div>
+			</section>
+			
+		</div>
+		
+
+	</header>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>index</title>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+	<link rel="stylesheet" href="css/header.css">
+	<link rel="stylesheet" href="css/home.css">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<?php  require_once("assets/header.php");?>
+
+	<main>
+		<div class="banner-home">
+			<p>Seja bem vindo(a) a ACME!</p>
+			<p>Aqui você encontra os melhores calçados pelo menor preço</p>
+		</div>
+	</main>
+</body>
+</html>
+
