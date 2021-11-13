3d4690b6f67df5ff581bc4058242e569378aaf89
gabriielsilva5 <dev.gbcproject@gmail.com>
2021-11-13 15:22:54 -0300

    ajuste no arquivo sendmail.php

--- config/sendmail.php
+++ config/sendmail.php
@@ -1,21 +1,12 @@
 <?php
 
-//// Email que vai aparecer como remetente (quem enviou)
-//const FROM_EMAIL = "";
-//// Nome que vai aparecer como remetente (quem enviou)
-//const FROM_NAME = "ACME Calçados";
-//
-//// SMTP username
-//const USER_NAME = "";
-//// SMTP password
-//const USER_PASSWD = "";
-
 // Email que vai aparecer como remetente (quem enviou)
-const FROM_EMAIL = "sendmail.php.tests@gmail.com";
+const FROM_EMAIL = "";
 // Nome que vai aparecer como remetente (quem enviou)
 const FROM_NAME = "ACME Calçados";
 
 // SMTP username
-const USER_NAME = "sendmail.php.tests@gmail.com";
+const USER_NAME = "";
 // SMTP password
-const USER_PASSWD = "2C11uc5F7K95qpD";
+const USER_PASSWD = "";
+
