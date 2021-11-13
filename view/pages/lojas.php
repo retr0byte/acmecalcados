15a90f9b3396232edf3c51929e53752f641f77a8
gabriielsilva5 <dev.gbcproject@gmail.com>
2021-11-12 21:43:39 -0300

    pagina nossas lojas #62

--- view/pages/lojas.php
+++ view/pages/lojas.php
@@ -5,10 +5,123 @@
     <?php  require_once PATH.'/../view/layout/global/default_head.php';?>
 
     <title>ACME Calçados | Lojas</title>
-    <link rel="stylesheet" href="<?php echo PATH_LINKS ?>/assets/css/sejaUmFranqueado.css">
+    <link rel="stylesheet" href="<?php echo PATH_LINKS ?>/assets/css/lojas.css">
 </head>
 <body>
 <?php  require_once PATH.'/../view/layout/global/header.php';?>
+
+    <main class="lojas">
+        <h1>Nossas lojas</h1>
+
+        <section class="img-wrapper"></section>
+
+        <section class="enderecos-lojas">
+            <div class="table-line">
+                <div class="table-column">
+                    <p>Santos</p>
+                    <p>Rua João Pessoa, Centro</p>
+                    <p><i class="fas fa-phone-alt"></i> (13) 3232-2323</p>
+                    <p><i class="fab fa-whatsapp"></i> (13) 99770-7070</p>
+
+                    <div>
+                        <button><i class="fas fa-calendar-alt"></i> Agendar</button>
+                    </div>
+                </div>
+
+                <div class="table-column">
+                    <p>Santos</p>
+                    <p>Rua João Pessoa, Centro</p>
+                    <p><i class="fas fa-phone-alt"></i> (13) 3232-2323</p>
+                    <p><i class="fab fa-whatsapp"></i> (13) 99770-7070</p>
+
+                    <div>
+                        <button><i class="fas fa-calendar-alt"></i> Agendar</button>
+                    </div>
+                </div>
+
+                <div class="table-column">
+                    <p>Santos</p>
+                    <p>Rua João Pessoa, Centro</p>
+                    <p><i class="fas fa-phone-alt"></i> (13) 3232-2323</p>
+                    <p><i class="fab fa-whatsapp"></i> (13) 99770-7070</p>
+
+                    <div>
+                        <button><i class="fas fa-calendar-alt"></i> Agendar</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="table-line">
+                <div class="table-column">
+                    <p>Santos</p>
+                    <p>Rua João Pessoa, Centro</p>
+                    <p><i class="fas fa-phone-alt"></i> (13) 3232-2323</p>
+                    <p><i class="fab fa-whatsapp"></i> (13) 99770-7070</p>
+
+                    <div>
+                        <button><i class="fas fa-calendar-alt"></i> Agendar</button>
+                    </div>
+                </div>
+
+                <div class="table-column">
+                    <p>Santos</p>
+                    <p>Rua João Pessoa, Centro</p>
+                    <p><i class="fas fa-phone-alt"></i> (13) 3232-2323</p>
+                    <p><i class="fab fa-whatsapp"></i> (13) 99770-7070</p>
+
+                    <div>
+                        <button><i class="fas fa-calendar-alt"></i> Agendar</button>
+                    </div>
+                </div>
+
+                <div class="table-column">
+                    <p>Santos</p>
+                    <p>Rua João Pessoa, Centro</p>
+                    <p><i class="fas fa-phone-alt"></i> (13) 3232-2323</p>
+                    <p><i class="fab fa-whatsapp"></i> (13) 99770-7070</p>
+
+                    <div>
+                        <button><i class="fas fa-calendar-alt"></i> Agendar</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="table-line">
+                <div class="table-column">
+                    <p>Santos</p>
+                    <p>Rua João Pessoa, Centro</p>
+                    <p><i class="fas fa-phone-alt"></i> (13) 3232-2323</p>
+                    <p><i class="fab fa-whatsapp"></i> (13) 99770-7070</p>
+
+                    <div>
+                        <button><i class="fas fa-calendar-alt"></i> Agendar</button>
+                    </div>
+                </div>
+
+                <div class="table-column">
+                    <p>Santos</p>
+                    <p>Rua João Pessoa, Centro</p>
+                    <p><i class="fas fa-phone-alt"></i> (13) 3232-2323</p>
+                    <p><i class="fab fa-whatsapp"></i> (13) 99770-7070</p>
+
+                    <div>
+                        <button><i class="fas fa-calendar-alt"></i> Agendar</button>
+                    </div>
+                </div>
+
+                <div class="table-column">
+                    <p>Santos</p>
+                    <p>Rua João Pessoa, Centro</p>
+                    <p><i class="fas fa-phone-alt"></i> (13) 3232-2323</p>
+                    <p><i class="fab fa-whatsapp"></i> (13) 99770-7070</p>
+
+                    <div>
+                        <button><i class="fas fa-calendar-alt"></i> Agendar</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 
 <?php  require_once PATH.'/../view/layout/global/footer.php'; ?>
 </body>
