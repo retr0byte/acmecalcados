b84770d893294ccdfefcc34857a0c5f20e83bae7
gabriielsilva5 <dev.gbcproject@gmail.com>
2021-11-25 23:55:37 -0300

    mudanca no import do fontawesome

--- view/layout/global/default_head.php
+++ view/layout/global/default_head.php
@@ -7,7 +7,7 @@
 <link rel="stylesheet" href="<?php echo PATH_LINKS ?>/assets/css/header.css">
 <link rel="stylesheet" href="<?php echo PATH_LINKS ?>/assets/css/footer.css">
 
-<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script src="https://unpkg.com/ionicons@4.4.2/dist/ionicons.js"></script>
 <script src="<?php echo PATH_LINKS; ?>/assets/libs/jQuery/jquery.min.js"></script>
 <script src="<?php echo PATH_LINKS; ?>/assets/js/highlight_menu.js"></script>
